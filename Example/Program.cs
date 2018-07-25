3cff883f435fb72b12c4eb3f2cfefbef9078205d
Jorge Turrado Ferrero <jturrado@stt-systems.com>
2018-07-25 11:25:16 +0200

    Added Example of usage

---
+++ Example/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using WakeOnLan;
+
+namespace Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a Mac Adddress");
+            string mac = Console.ReadLine();
+            WakeOnLanClient WOLClient = new WakeOnLanClient();
+            WOLClient.Wake(mac);
+            Console.WriteLine("Pulse any key to exit...");
+            Console.Read();
+        }
+    }
+}
