b65c996bfc8cb96e20dc7d4a869485887235fa8d
Jorge Turrado Ferrero <jturrado@stt-systems.com>
2018-08-10 12:27:03 +0200

    Added Support to Async Wake

--- src/EasyWakeOnLan/EasyWakeOnLanClient.cs
+++ src/EasyWakeOnLan/EasyWakeOnLanClient.cs
@@ -2,13 +2,14 @@
 using System.Net;
 using System.Net.Sockets;
 using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace EasyWakeOnLan
 {
     /// <summary>
     /// Class to Send Magic Packet to Wake On Lan
     /// </summary>
-    public class EasyWakeOnLanClient : UdpClient
+    public class EasyWakeOnLanClient : UdpClient, IEasyWakeOnLanCient
     {
         public EasyWakeOnLanClient() : base()
         {
@@ -45,6 +46,38 @@ namespace EasyWakeOnLan
             //now send wake up packet
             IPEndPoint destiny = new IPEndPoint(IPAddress.Broadcast, 40000);
             SendAsync(Bytes, 1024, destiny);
+        }
+        /// <summary>
+        /// Wake a PC Async
+        /// </summary>
+        /// <param name="Mac">NIC Mac to wake</param>
+        public async Task WakeAsync(string Mac)
+        {
+            //Parse the mac
+            var MacParsed = Regex.Replace(Mac, "[-|:]", "");
+
+            //set sending bites
+            int Counter = 0;
+            //buffer to be send
+            byte[] Bytes = new byte[1024];   // more than enough :-)
+                                             //first 6 bytes should be 0xFF
+            for (int y = 0; y < 6; y++)
+                Bytes[Counter++] = 0xFF;
+            //now repeate MAC 16 times
+            for (int y = 0; y < 16; y++)
+            {
+                int i = 0;
+                for (int z = 0; z < 6; z++)
+                {
+                    Bytes[Counter++] =
+                        byte.Parse(MacParsed.Substring(i, 2),
+                        NumberStyles.HexNumber);
+                    i += 2;
+                }
+            }
+            //now send wake up packet
+            IPEndPoint destiny = new IPEndPoint(IPAddress.Broadcast, 40000);
+            await SendAsync(Bytes, 1024, destiny);
         }
     }
 }
---
+++ src/EasyWakeOnLan/IEasyWakeOnLanClient.cs
@@ -0,0 +1,23 @@
+﻿
+using System.Threading.Tasks;
+
+namespace EasyWakeOnLan
+{
+    /// <summary>
+    /// EasyWakeOnLanCient interface
+    /// </summary>
+    public interface IEasyWakeOnLanCient
+    {
+        /// <summary>
+        /// Wake a PC
+        /// </summary>
+        /// <param name="Mac">NIC Mac to wake</param>
+        void Wake(string Mac);
+        /// <summary>
+        /// Wake a PC Async
+        /// </summary>
+        /// <param name="Mac">NIC Mac to wake</param>
+        Task WakeAsync(string Mac);
+
+    }
+}
