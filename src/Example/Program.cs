8dd3399be72b59500e152da5f0a7baf72056ef43
Jorge Turrado Ferrero <jturrado@stt-systems.com>
2018-08-10 12:33:53 +0200

    Add async metod to Example

--- src/Example/Program.cs
+++ src/Example/Program.cs
@@ -7,10 +7,17 @@ namespace Example
     {
         static void Main(string[] args)
         {
+            Process();
+        }
+        static async void Process()
+        {
             Console.WriteLine("Enter a Mac Adddress");
             string Mac = Console.ReadLine();
             EasyWakeOnLanClient WOLClient = new EasyWakeOnLanClient();
+            //Sync
             WOLClient.Wake(Mac);
+            //Async
+            await WOLClient.WakeAsync(Mac);
             Console.WriteLine("Pulse any key to exit...");
             Console.Read();
         }
